154feb74f9dc5f1478db851efcd45d05dab9caf2
valentina.gorskova <valentina.gorskova@x5.ru>
2020-09-01 22:28:26 +0300

    add file task_3

---
+++ src/lesson_2/task_3.java
@@ -0,0 +1,21 @@
+package lesson_2;
+import java.util.Scanner;
+
+public class task_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        if (scanner.hasNextInt()) {
+            int i = scanner.nextInt();
+            if (i==0) {
+                System.out.println("You entered zero!");
+            }
+            else
+                if (i<0) {
+                    System.out.println("You entered:" + i);
+                }
+                else System.out.printf("Result: %d", ++i);
+        }
+        else System.out.println("Entered is not an integer!");
+    }
+}
