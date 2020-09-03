6a5543826fd323be57d4225003d5ba498367fe93
valentina.gorskova <valentina.gorskova@x5.ru>
2020-09-03 21:52:25 +0300

    add file task_10

---
+++ src/lesson_2/task_10.java
@@ -0,0 +1,21 @@
+package lesson_2;
+
+import java.util.Arrays;
+
+public class task_10 {
+    public static void main(String[] args) {
+        int[] array = {15, 10, 0, -6, -5, 3, 0, -34, 0, 32, 56, 0, 24, -52};
+
+        int i = 0;
+
+        for ( int j = 0; j < array.length; j++) {
+            if (array[j] != 0) {
+                array[i++] = array[j];
+            }
+        }
+        while (i < array.length)
+            array[i++]=0;
+
+        System.out.println("Преобразованный массив: " + Arrays.toString(array));
+    }
+}
