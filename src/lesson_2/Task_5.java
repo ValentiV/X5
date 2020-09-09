c90155ff1ccb97cd16b9f3cf316320ab9e47c32b
valentina.gorskova <valentina.gorskova@x5.ru>
2020-09-09 19:20:06 +0300

    changes

--- src/lesson_2/Task_5.java
+++ src/lesson_2/Task_5.java
@@ -5,14 +5,23 @@ import java.util.Scanner;
 public class Task_5 {
     public static void main(String[] args) { // Определяем наименьшее число из трех введенных
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a three integer: ");
-        int i = scanner.nextInt();
-        int j = scanner.nextInt();
-        int k = scanner.nextInt();
-        if (i <= j && i <= k) { //равно на случай, если введены одинаковые числа
-            System.out.println("Minimum number is: " + i);
-        } else if (j <= i && j <= k) {
-            System.out.println("Minimum number is: " + j);
-        } else System.out.println("Minimum number is: " + k);
+
+        System.out.print("Enter a size of massive: ");
+        int s = scanner.nextInt();
+        int[] array = new int[s];
+
+        for (int i = 0; i < s; i++) {
+            System.out.println("Enter an integer: ");
+            array[i] = scanner.nextInt(); // Заполняем массив элементами, введёнными с клавиатуры
+        }
+        System.out.println("Minimum number is: " + min(array));
+    }
+
+    public static int min(int[] array) {
+        int min = array[0];
+        for (int j : array) {
+            if (min > j) min = j;
+        }
+        return min;
     }
 }
