69f0fd78c756e96698e7ebda4be68f5556fa9bbe
Valentina Gorskova <gorskovavalya@yandex.ru>
2020-09-21 21:09:57 +0300

    начало

---
+++ src/lesson_7/Account.java
@@ -0,0 +1,7 @@
+package lesson_7;
+
+public class Account {
+    private int id;
+    private String holder;
+    private double amount;
+}
---
+++ src/lesson_7/AccountService.java
@@ -0,0 +1,12 @@
+package lesson_7;
+
+public interface AccountService {
+    <NotEnoughMoneyException extends Throwable, UnknownAccountException extends Throwable>
+    void withdraw(int accountId, int amount) throws NotEnoughMoneyException, UnknownAccountException;
+
+    void balance(int accountId) throws UnknownAccountException;
+
+    void deposit(int accountId, int amount) throws NotEnoughMoneyException, UnknownAccountException;
+
+    void transfer(int from, int to, int amount) throws NotEnoughMoneyException, UnknownAccountException;
+}
